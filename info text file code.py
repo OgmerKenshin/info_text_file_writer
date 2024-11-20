d8c5699da4ab30c237dfce29e7e1688deec80686
OgmerKenshin <kenshinaogmer@gmail.com>
2024-11-20 18:06:40 +0800

    another value holder for user's number

--- info text file code.py
+++ info text file code.py
@@ -13,6 +13,7 @@ while True:
         try:
             name = input("enter your name: ")
             age = int(input("enter your ageZ: "))
+            number = int(input("enter your number: "))
 
 
 
