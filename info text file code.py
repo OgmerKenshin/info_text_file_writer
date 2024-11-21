9d09ab003f5a4837a43de030a11b0d95ade6231d
OgmerKenshin <kenshinaogmer@gmail.com>
2024-11-21 21:33:31 +0800

    I added an \n to see if it would organize the inputs

--- info text file code.py
+++ info text file code.py
@@ -35,7 +35,7 @@ while True:
             }
 
             with open("c:\\Users\\kensh\\OneDrive - DepEd-NCR\\coding assignment text file\\text_file.py", "a") as file_handler:
-                file_handler.write(str(person[IGN]))
+                file_handler.write("\n" + str(person[IGN]))
         
 
             print("your input has been saved, welcome to mobile legends")
