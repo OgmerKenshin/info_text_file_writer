9630e46becb073451bf2408f5df1df829a70ee44
OgmerKenshin <kenshinaogmer@gmail.com>
2024-11-22 14:56:29 +0800

    I'm gonna start over and make a new output file

--- info_text_file_code.py
+++ info_text_file_code.py
@@ -10,22 +10,13 @@ import json
 file_hold = "output.json"
 
 IGN_input = input("enter your in game name: ")
-try:
-     with open(file_hold, "r") as file:
+
+with open(file_hold, "r") as file:
         info = json.load(file)
-        print("info is loaded now")
 
-except FileNotFoundError:
-    print("file not found, creating new file")
-    info = {}
-    with open (file_hold, "w") as file:
-        json.dump(info, file)
-
-except json.JSONDecodeError:
-    with open(file_hold, "w") as file: 
-        json.dump(info, file)
-
-
-
-    
+for IGN in info:
+        if IGN_input == info[IGN]:
+            print(f"Here is the data for {IGN}: {IGN_input}")
+        else: 
+            print("no input info for IGN")
 
