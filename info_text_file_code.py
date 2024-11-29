5b11732dbd67e44c7f9be57be9ed521dc9c8b448
OgmerKenshin <kenshinaogmer@gmail.com>
2024-11-29 17:22:23 +0800

    never mind that was an even worse Idea

--- info_text_file_code.py
+++ info_text_file_code.py
@@ -4,41 +4,38 @@
 #open and read json file
 #search for the user input
 # handle errors
-info_dictionary = {}
+
 file_path = "output.txt"
-
-with open(file_path, "r") as file:
-      lines = file.readlines()
-      info = []
-
-      for i, line in enumerate(lines):
-        line= line.strip()
-
-      if line.startswith(f"IGN:"): 
-            if info:
-               ign = info[0].split(": ")[1]
-               info_dictionary[ign] = info
-            info = [line]
-                    
-      else:
-        info.append(line)
-
-      if info:
-        ign = info[0].split(": ")[1]
-        info_dictionary[ign] = info
-        
 while True:
-      try: 
-         search_ign = input("enter an IGN? : ")
-      
+    search_ign = input("enter your IGN: ")
+
+
+    searched = False
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+        info = []
+
+        for i, line in enumerate(lines):
+          line= line.strip()
+
+          if line.startswith(f"IGN: ") and not line.startswith(f"IGN: "): 
+                searched = True
+                info.append(line.strip())
+
+                for subsequent_line in lines[i + 1:]:
+                     if subsequent_line.startswith("IGN:"):
+                          break
+                     info.append(subsequent_line.strip())
+
+        
       
                 
-      if searched == True:
-          print(f"info on {search_ign}")
-          print(f"\n".join(info))
+          if searched == True:
+            print(f"info on {search_ign}")
+            print(f"\n".join(info))
             
-      else:
-          print(f"no info for {search_ign}")
+          else:
+            print(f"no info for {search_ign}")
             
                
       
